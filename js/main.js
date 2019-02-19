d513881d2b0fdeddcaf9f4cbc73c2f8cd8c9c34f
JAguVillar <jaguvillar@gmail.com>
2019-02-19 09:21:56 -0300

    avance 1902

--- js/main.js
+++ js/main.js
@@ -2,63 +2,56 @@ var longitud;
 var latitud;
 
 if (navigator.geolocation) {
- navigator.geolocation.getCurrentPosition(localizacion);
+    navigator.geolocation.getCurrentPosition(localizacion);
 } else {
- alert("Fuck");
+    alert("Fuck");
 }
 
 function localizacion(position) {
- longitud = position.coords.longitude;
- latitud = position.coords.latitude;
- let proxy = `https://cors-anywhere.herokuapp.com/`;
- let apiURL = `${proxy}https://api.darksky.net/forecast/9155007309d8bd89b080c1921f5d1ac1/${latitud}, ${longitud}?lang=es&units=si`;
+    longitud = position.coords.longitude;
+    latitud = position.coords.latitude;
+    let proxy = `https://cors-anywhere.herokuapp.com/`;
+    let apiURL = `${proxy}https://api.darksky.net/forecast/9155007309d8bd89b080c1921f5d1ac1/${latitud}, ${longitud}?lang=es&units=si`;
 
- $.getJSON(apiURL, function (data) {
-  actualizar(data);
-  console.log(data);
+    $.getJSON(apiURL, function (data) {
+        actualizar(data);
+        console.log(data);
 
- });
+    });
 
- function actualizar(data) {
-  var fecha = new Date();
-  var diaNumero = fecha.getDate();
-  var diaSemana = fecha.getDay();
-  var fechaUnix = new Date(data.currently.time * 1000);
-  var hora = fechaUnix.getHours();
-  var minutos = "0" + fechaUnix.getMinutes();
-  var segundos = "0" + fechaUnix.getSeconds();
-  console.log(hora + ":" + minutos.substr(-2) + ":" + segundos.substr(-2));
+    function actualizar(data) {
+        var fecha = new Date();
+        var diaNumero = fecha.getDate();
+        var diaSemana = fecha.getDay();
+        var fechaUnix = new Date(data.currently.time * 1000);
+        var hora = fechaUnix.getHours();
+        var minutos = "0" + fechaUnix.getMinutes();
+        var segundos = "0" + fechaUnix.getSeconds();
+        console.log(hora + ":" + minutos.substr(-2) + ":" + segundos.substr(-2));
 
-  var dia = new Array();
-  dia[0] = "Domingo";
-  dia[1] = "Lunes";
-  dia[2] = "Martes";
-  dia[3] = "Miercoles";
-  dia[4] = "Jueves";
-  dia[5] = "Viernes";
-  dia[6] = "Sabado";
+        var dia = new Array();
+        dia[0] = "Domingo";
+        dia[1] = "Lunes";
+        dia[2] = "Martes";
+        dia[3] = "Miercoles";
+        dia[4] = "Jueves";
+        dia[5] = "Viernes";
+        dia[6] = "Sabado";
 
-  $("#fecha").html(dia[diaSemana] + ` ` + diaNumero);
+        $("#fecha").html(dia[diaSemana] + ` ` + diaNumero);
 
-  $("#temperatura").html(Math.round(data.currently.temperature) + ` °C`);
+        $("#temperatura").html(Math.round(data.currently.temperature) + ` °C`);
 
-  var nombreArchivo = data.currently.icon;
-  $("#icono").html(`<img id="iconoMain" src="iconos/` + nombreArchivo + `.svg"/>`);
+        var nombreArchivo = data.currently.icon;
+        $("#icono").html(`<img id="iconoMain" src="iconos/` + nombreArchivo + `.svg"/>`);
 
-  $("#pronostico").html(data.currently.summary);
+        $("#pronostico").html(data.currently.summary);
 
-  var info = `<div`;
-
-  info +=
-   ` id="humedad"><i class='uil uil-tear'></i> Humedad: ` +
-   Math.round(data.currently.humidity * 100) + `</div>`;
-  info +=
-   `<div div="precipitacion"><i class='uil uil-umbrella'></i> Probabilidad de Precipitacion: ` +
-   Math.round(data.currently.precipProbability * 100) + `</div>`;
-  info +=
-   `<div id="viento"><i class='uil uil-wind'></i> Viento: ` +
-   data.currently.windSpeed +
-   ` km/h.</div>`;
-  $(".contenedorInfo").html(info);
- }
+        $(".contenedorHumedad").html(`<div id="humedad" style="margin: 10px"><i class='uil uil-tear'></i> Humedad: ` +
+            Math.round(data.currently.humidity * 100) + `%</div>`);
+        $(".contenedorPrecipitacion").html(`<div id="precipitacion" style="margin: 10px"><i class='uil uil-umbrella'></i> Probabilidad de Precipitacion: ` +
+            Math.round(data.currently.precipProbability * 100) + `%</div>`);
+        $(".contenedorViento").html(`<div id="viento" style="margin: 10px"><i class='uil uil-wind'></i> Viento: ` +
+            data.currently.windSpeed + ` km/h.</div>`);
+    }
 }
