6c8bd2fb35042d018207a25ff15d754c5ecfff02
JAguVillar <jaguvillar@gmail.com>
2019-02-21 02:31:30 -0300

    casi todo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+var placesAutocomplete = places({
+    appId: 'plU99I6Y3I2Z',
+    apiKey: '1cf647b42db5b6fc90b9892b48b04557',
+    container: document.querySelector('#address-input'),
+    type: 'city', // Search only for cities names
+    aroundLatLngViaIP: false, // disable the extra search/boost around the source IP
+    useDeviceLocation: true
+});
+
+placesAutocomplete.on('change', function (e) {
+    console.log(e.suggestion);
+});
+
+
 var longitud;
 var latitud;
 
@@ -15,8 +29,8 @@ function localizacion(position) {
 
     $.getJSON(apiURL, function (data) {
         actualizar(data);
+        actualizarPost(data);
         console.log(data);
-
     });
 
     function actualizar(data) {
@@ -51,9 +65,76 @@ function localizacion(position) {
 
         $(".contenedorHumedad").html(`<div id="humedad" style="margin: 10px"><i class='uil uil-tear'></i> Humedad: ` +
             Math.round(data.currently.humidity * 100) + `%</div>`);
-        $(".contenedorPrecipitacion").html(`<div id="precipitacion" style="margin: 10px"><i class='uil uil-umbrella'></i> % Precipitacion: ` +
+        $(".contenedorPrecipitacion").html(`<div id="precipitacion" style="margin: 10px"><i class='uil uil-umbrella'></i> %Precipitacion: ` +
             Math.round(data.currently.precipProbability * 100) + `%</div>`);
         $(".contenedorViento").html(`<div id="viento" style="margin: 10px"><i class='uil uil-wind'></i> Viento: ` +
             data.currently.windSpeed + ` km/h.</div>`);
+    }
+
+    function actualizarPost(data) {
+        var d = 1;
+
+        while (d <= 7) {
+            var fechaUnix = data.daily.data[d].time; // UNIX timestamp in seconds
+            var diaNumero = new Date();
+            diaNumero.setTime(fechaUnix * 1000); // javascript timestamps are in milliseconds
+            diaNumero.toUTCString();
+
+            var fechaUnix = new Date(data.daily.data[d].time * 1000);
+            var hora = fechaUnix.getHours();
+            var minutos = "0" + fechaUnix.getMinutes();
+            var segundos = "0" + fechaUnix.getSeconds();
+            var horaPronostico = hora + ":" + minutos.substr(-2);
+
+            var dia = new Array();
+            dia[0] = "Domingo";
+            dia[1] = "Lunes";
+            dia[2] = "Martes";
+            dia[3] = "Miercoles";
+            dia[4] = "Jueves";
+            dia[5] = "Viernes";
+            dia[6] = "Sabado";
+
+            var post = `<div class="contenedorDia">`;
+
+            post +=
+                `<div id="iconoPost">
+                <img src="iconos/` + data.daily.data[d].icon + `.svg" alt="">
+                </div>`;
+            if (d == 1) {
+                post +=
+                    `<div id="diaPost">
+                    ` + `Mañana` + `
+                    </div>`;
+            } else {
+                post +=
+                    `<div id="diaPost">
+                    ` + dia[diaNumero.getDay()] + `
+                    </div>`;
+            }
+
+            post +=
+                `<div id="pronosticoPost">
+                    ` + data.daily.data[d].summary + `
+                    </div>`;
+
+            if (data.daily.data[d].icon == "rain") {
+                post +=
+                    `<div id="lluviaPost">
+                        <i class='uil uil-umbrella'></i>` +
+                    `<p>` + Math.round(data.daily.data[d].precipProbability * 100) +
+                    `%</p>` +
+                    `</div>`;
+            } else {
+                post +=
+                    `<div id="lluviaPost"> </div>`;
+            }
+
+            post +=
+                `<div id=contenedorMax>Max.<br>` + Math.round(data.daily.data[d].temperatureMax) + `°C</div>` +
+                `<div id=contenedorMin>Min.<br>` + Math.round(data.daily.data[d].temperatureMin) + `°C</div>`;
+            d++;
+            $(".contenedorPronosticos").append(post);
+        }
     }
 }
